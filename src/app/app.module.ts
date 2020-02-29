f533fb977d507afbc8137d6fc8a8107502593698
Danil Makushev <danil.makushev@outlook.com>
2020-02-29 13:31:28 +0100

    lay out project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {NavigationModule} from './navigation/navigation.module';
+import {ShoppingModule} from './shopping/shopping.module';
+import {ProjectModule} from './project/project.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NavigationModule,
+    ShoppingModule,
+    ProjectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/project/project.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProjectListComponent } from './project-list/project-list.component';
+import { ProjectItemComponent } from './project-item/project-item.component';
+
+
+
+@NgModule({
+  declarations: [ProjectListComponent, ProjectItemComponent],
+  imports: [
+    CommonModule
+  ],
+   exports: [ProjectListComponent]
+})
+export class ProjectModule { }
